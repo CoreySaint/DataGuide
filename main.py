c23b00ebc2d8e86aeba3de1a2654caa0b60df4d2
Corey St Jean <115374534+CoreySaint@users.noreply.github.com>
2025-04-16 16:59:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ from DataGuide import DataGuide, Node
 
 guide = DataGuide()
 
-doc = {'a': 1, 'b': {'c': 'foo', 'd': 'bar', 'e': 4}, 'f': [1, 2, 3]}
+data = {'a': 1, 'b': {'c': 'foo', 'd': 'bar', 'e': 4}, 'f': [1, 2, 3]}
 
-guide.insert_document(doc)
+guide.insert_document(data)
 
 guide.print_guide()
+print()
 
 print(guide.search('b.c'))
+print()
 
-guide.delete_document(doc)
+guide.delete_document(data)
 
 guide.print_guide()
+print()
 
+with open("example_2.json", "r") as f:
+  data = json.load(f)
+
+guide.insert_document(data)
+
+guide.print_guide()
