5ac1674ebe31ede5152ba495bd6e541b2790dd3f
Corey St Jean <115374534+CoreySaint@users.noreply.github.com>
2025-04-28 16:24:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from DataGuide_v4 import DataGuide, Node
 
+#Testing basic functionality
 guide = DataGuide()
 
 data = {'a': 1, 'b': {'c': 'foo', 'd': 'bar', 'e': 4}, 'f': [1, 2, 3]}
@@ -18,6 +19,7 @@ guide.delete_document(data)
 guide.print_guide()
 print()
 
+#Testing file input
 with open("example_2.json", "r") as f:
   data = json.load(f)
 
@@ -26,6 +28,7 @@ guide.insert_document(data)
 guide.print_guide()
 print()
 
+#Testing card and core methods
 coreguide = guide.core()
 
 coreguide.print_guide()
@@ -34,3 +37,20 @@ print()
 cardguide = guide.card()
 
 cardguide.print_guide()
+
+#Testing edge cases
+edge_cases = DataGuide()
+
+with open("EdgeCases.json", "r") as g:
+  data_g = json.load(g)
+
+edge_cases.insert_document(data_g)
+
+edge_cases.print_guide()
+print()
+
+edge_core = edge_cases.core()
+edge_code.print_guide()
+print()
+
+print(edge_cases.total_docs)
