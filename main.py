57ce335a177e943684e45632945362486541d1e2
Corey St Jean <115374534+CoreySaint@users.noreply.github.com>
2025-05-14 13:10:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -55,3 +55,38 @@ edge_code.print_guide()
 print()
 
 print(edge_cases.total_docs)
+
+data1 = {"a": 2, "b": {"c": 7, "d": 'bar', "e": 4}, "f": [1, 2, 3]}
+data2 = {"a": 2, "b": {"c": 'bar'}, "g": 7}
+
+guide1 = DataGuide()
+guide2 = DataGuide()
+
+guide1.insert_document(data1)
+guide2.insert_document(data2)
+
+union = guide1.union(guide2)
+union.print_guide()
+print()
+
+data1 = [
+    { "a": 1, "common": { "x": 10 }, "u1": "foo" },
+    { "a": 2, "common": { "x": "twenty" }, "u2": "bar" },
+    { "a": 3, "common": { "x": 30 } }
+]
+
+data2 = [
+    { "a": 100, "common": { "x": 40 }, "v1": 3.14 },
+    { "a": 200, "common": { "x": 20 }},
+    { "a": 300, "common": { "x": "gamma" }, "v2": 2.71 }
+]
+
+guide3 = DataGuide()
+guide4 = DataGuide()
+
+guide3.insert_document(data1)
+guide4.insert_document(data2)
+
+difference = guide3.difference(guide4)
+difference.print_guide()
+print()
